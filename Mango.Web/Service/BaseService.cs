1b837641674a8919072b6fe2bf61deed9b6aa899
Geo Manuel George <125736862+geo-man-geo@users.noreply.github.com>
2024-02-27 16:33:34 +0530

    Add project files.

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,113 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static Mango.Web.Models.Utilities.SD;
+
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        { 
+            _httpClientFactory = httpClientFactory;
+
+        }
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+        try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+
+                //token
+                if (requestDto.Url == null)
+                {
+                    throw new ArgumentNullException(nameof(requestDto.Url), "URL cannot be null");
+                }
+
+
+
+                message.RequestUri = new Uri(requestDto.Url);
+
+            if (requestDto.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+            }
+
+            HttpResponseMessage? apiResponse = null;
+
+            switch(requestDto.ApiType)
+            {
+                case ApiType.POST:  
+                    message.Method = HttpMethod.Post;   
+                    break;
+                case ApiType.PUT: 
+                    message.Method = HttpMethod.Put;    
+                    break;
+                case ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+
+            apiResponse = await client.SendAsync(message);
+
+                if (apiResponse != null)
+                {
+                    switch (apiResponse.StatusCode)
+                    {
+                        case HttpStatusCode.NotFound:
+                            return new() { IsSuccess = false, Message = "Not Found" };
+
+                        case HttpStatusCode.Unauthorized:
+                            return new() { IsSuccess = false, Message = "Unauthorized" };
+
+                        case HttpStatusCode.BadRequest:
+                            return new() { IsSuccess = false, Message = "Bad Request" };
+
+                        case HttpStatusCode.Forbidden:
+                            return new() { IsSuccess = false, Message = "Forbidden" };
+
+                        case HttpStatusCode.InternalServerError:
+                            return new() { IsSuccess = false, Message = "Internal Server Error" };
+
+                        case HttpStatusCode.BadGateway:
+                            return new() { IsSuccess = false, Message = "Bad Gateway" };
+
+                        case HttpStatusCode.ServiceUnavailable:
+                            return new() { IsSuccess = false, Message = "Service Unavailable" };
+
+                        case HttpStatusCode.GatewayTimeout:
+                            return new() { IsSuccess = false, Message = "Gateway Timeout" };
+
+                        default:
+                            var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                            var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                            return apiResponseDto;
+
+                    }
+                }
+                else
+                {
+                    return new ResponseDto() { IsSuccess = false, Message = "API Response is null" };
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto()
+                {
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false
+                };
+            return dto;
+            }
+        }
+    }
+}
---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,70 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Models.Utilities;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto> AddCouponAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Url = SD.CouponAPIBase + "/api/couponAPI/",
+                Data = couponDto
+            }) ;
+        }
+
+        public async Task<ResponseDto> DeleteCouponAsync(int couponId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = SD.CouponAPIBase + "/api/couponAPI/" + couponId
+            });
+        }
+
+        public async Task<ResponseDto> GetAllCouponAsync()
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/couponAPI"
+            });
+        }
+
+        public async Task<ResponseDto> GetCouponAsync(string couponCode)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/couponAPI/GetByCode" + couponCode
+            }) ;
+        }
+
+        public async Task<ResponseDto> GetCouponByIdAsync(int couponId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/couponAPI/" + couponId
+            });
+        }
+
+        public async Task<ResponseDto> UpdateCouponAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.PUT,
+                Url = SD.CouponAPIBase + "/api/couponAPI/",
+                Data = couponDto
+            });
+        }
+    }
+}
